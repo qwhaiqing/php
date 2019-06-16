ccea38de5197300729a299fd1fc11449a81745a2
qwhaiqing <51876753+qwhaiqing@users.noreply.github.com>
2019-06-16 17:34:17 +0800

    yqlb.php
    需要插入替换的列表

---
+++ yqlb.php
@@ -0,0 +1,14 @@
+	<li class="js_songlist__child" mid="639141" ix="0">
+	<div class="songlist__item" onmouseover="this.className=(this.className+' songlist__item--hover')" onmouseout="this.className=this.className.replace(/ songlist__item--hover/, '')">
+	<div class="songlist__edit songlist__edit--check sprite">
+	<input type="checkbox" class="songlist__checkbox">
+	</div>
+	<div class="songlist__songname">
+	<span class="songlist__songname_txt"><a href="urlgeming" class="js_song" title="geming"><span class="c_tx_highlight">geming</span> </a></span><!--歌名-->
+	<span class="songlist__child_num" style="display:none;">(7)</span>
+	 </div>
+	<div class="songlist__artist" title="geshou">
+	<a href="urlgeshou" data-singermid="id" data-singerid="11921" title="geshou" class="singer_name">geshou</a><!--歌手-->
+	</div>
+	</div>
+	</li>
